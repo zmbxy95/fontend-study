f25a73a57c8771f8118b91596855451a872add21
zmbxy95 <zmbxy95@gmail.com>
2021-01-25 08:20:34 +0800

    update

--- part-01/work-05/MyPromise.js
+++ part-01/work-05/MyPromise.js
@@ -1,18 +1,95 @@
+const STATE = {
+    PENDING: 'pending',
+    FULFILLED: 'fulfilled',
+    REJECTED: 'rejected'
+}
+
 class MyPromise {
 
     // 构造函数需要一个执行器函数，用于修改Promise的状态
-    constructor(executer) {
+    constructor(executor) {
         // 执行器函数会立即执行
-        executer(resolve, reject);
+        executor(this.resolve, this.reject)
     }
 
-    // resolve 函数需要一个value
-    resolve(value) {
+    // Promise状态：pending、fulfilled、rejected
+    _state = STATE.PENDING;
 
+    // 成功的值
+    _value = undefined;
+
+    // 失败/拒绝的原因
+    _reason = undefined;
+
+    // 成功回调
+    _successCallback = [];
+
+    // 失败回调
+    _failedCallback = [];
+
+    // resolve 函数需要一个value
+    resolve = (value) => {
+        if (this._state !== STATE.PENDING) {
+            return;
+        }
+
+        this._state = STATE.FULFILLED;
+        this._value = value;
+
+        // this.successCallback && this.successCallback(value);
+        let successCallback;
+        while ((successCallback = this._successCallback.shift())) {
+            let v = successCallback(value);
+        }
     }
 
     // reject 函数需要一个reason
-    reject(reason) {
+    reject = (reason) => {
+        if (this._state !== STATE.PENDING) {
+            return;
+        }
+        this._state = STATE.FULFILLED;
+        this._reason = reason;
 
+        // this.failedCallback && this.failedCallback(reason);
+        let failedCallback;
+        while ((failedCallback = this._failedCallback.shift())) {
+            failedCallback(reason);
+        }
+    }
+
+    // then方法返回一个promise对象
+    // 对于异步的then方法，需要等待Promise完成
+    then = (successCallback, failedCallback) => {
+
+        let promise = new MyPromise((resolve, reject) => {
+            // 成功
+            if (this._state === STATE.FULFILLED) {
+                let value = successCallback(this._value);
+                resolvePromise(value, resolve, reject);
+                return;
+            }
+            // 失败
+            if (this._state === STATE.REJECTED) {
+                let reason = failedCallback(this._reason);
+                reject(reason);
+                return;
+            }
+            // 等待 -> 待状态变成fullfiled或者rejected时调用
+            this._successCallback.push(successCallback);
+            this._failedCallback.push(failedCallback);
+        })
+
+        return promise;
     }
 }
+
+function resolvePromise(x, resolve, reject) {
+    if (x instanceof MyPromise) {
+        x.then(resolve, reject)
+    } else {
+        resolve(x);
+    }
+}
+
+module.exports = MyPromise;
--- part-01/work-05/work-05.js
+++ part-01/work-05/work-05.js
@@ -1,28 +1,138 @@
 // Promsie 三种状态: pendding(等待)、 fulfilled(完成)、 rejected(拒绝)
 // 状态切换只能由 pending -> fulfilled 或者 pending -> rejected
+// Promise状态一经改变就不可更改
 // Promise构造函数需要一个执行器函数，并且该执行器函数会立即执行
 // 同时会给执行器函数传递两个参数：resolve 和 reject 函数，用于修改Promise的状态
 // resolve函数需要一个value，reject需要一个reason
-
-let promise = new Promise(function (resolve, reject) {
-    setTimeout(function() {
-        let num = Math.floor(Math.random() * 10);
-        if (num % 2 === 0) {
-            resolve(num);
-        } else {
-            reject('num % 2 !== 0');
-        }
-    }, 3000);
-})
+// let promise = new Promise(function (resolve, reject) {
+//     setTimeout(function () {
+//         let num = Math.floor(Math.random() * 10);
+//         if (num % 2 === 0) {
+//             resolve(num);
+//         } else {
+//             reject('num % 2 !== 0');
+//         }
+//     }, 3000);
+// })
 
 // then方法是用来添加Promise状态明确后的回调函数
 // then方法需要两个函数：onFulfilled 和 onRejected函数，onRejected可以省略
 // then方法内部会返回一个Promise对象
-promise.then(
-    function onFulfilled(value) {
-        console.log(value);
-    },
-    function onRejected(reason) {
-        console.log(reason);
-    }
-)
+// promise.then(
+//     function onFulfilled(value) {
+//         console.log('---------- Promise success ----------')
+//         console.log(value);
+//     },
+//     function onRejected(reason) {
+//         console.log('---------- Promise failed ----------')
+//         console.log(reason);
+//     }
+// )
+
+
+// then 方法可以调用多次
+// promise.then(
+//     function onFulfilled(value) {
+//         console.log('---------- Promise success 1 ----------')
+//         console.log(value);
+//     },
+//     function onRejected(reason) {
+//         console.log('---------- Promise failed 1 ----------')
+//         console.log(reason);
+//     }
+// )
+
+// promise.then(
+//     function onFulfilled(value) {
+//         console.log('---------- Promise success 2 ----------')
+//         console.log(value);
+//     },
+//     function onRejected(reason) {
+//         console.log('---------- Promise failed 2 ----------')
+//         console.log(reason);
+//     }
+// )
+
+// then方法的链式调用
+// 原因就是then方法可以返回一个Promise对象
+// promise.then(function (value) {
+//     console.log('---------- Promise chain callback success 1 ----------')
+//     console.log(value);
+//     return Promise.resolve('HELLO ~')
+// }).then(function (value) {
+//     console.log('---------- Promise chain callback success 2 ----------')
+//     console.log(value);
+//     return 100
+// }).catch(function (reason) {
+//     console.log('---------- Promise chain callback failed 1 ----------')
+//     console.log(reason);
+// }).catch(function (reason) {
+//     console.log('---------- Promise chain callback failed 2 ----------')
+//     console.log(reason);
+// })
+
+const MyPromise = require('./MyPromise');
+
+let myPromise = new MyPromise(function (resolve, reject) {
+    // resolve(123);
+    // reject(456);
+    setTimeout(function () {
+        let num = Math.floor(Math.random() * 10);
+        resolve(num);
+        // if (num % 2 === 0) {
+        //     resolve(num);
+        // } else {
+        //     reject('num % 2 !== 0');
+        // }
+    }, 3000);
+});
+
+// myPromise.then(
+//     function onFulfilled(value) {
+//         console.log('---------- MyPromise success ----------')
+//         console.log(value);
+//     },
+//     function onRejected(reason) {
+//         console.log('---------- MyPromise failed ----------')
+//         console.log(reason);
+//     }
+// )
+
+// myPromise.then(
+//     function onFulfilled(value) {
+//         console.log('---------- MyPromise success 1 ----------')
+//         console.log(value);
+//     },
+//     function onRejected(reason) {
+//         console.log('---------- MyPromise failed 1 ----------')
+//         console.log(reason);
+//     }
+// )
+
+// myPromise.then(
+//     function onFulfilled(value) {
+//         console.log('---------- MyPromise success 2 ----------')
+//         console.log(value);
+//     },
+//     function onRejected(reason) {
+//         console.log('---------- MyPromise failed 2 ----------')
+//         console.log(reason);
+//     }
+// )
+
+// then方法的链式调用
+// 原因就是then方法可以返回一个Promise对象
+myPromise.then(function (value) {
+    console.log('---------- MyPromise chain callback success 1 ----------')
+    console.log(value);
+    return new MyPromise(function(resolve, reject) {
+        resolve('HELLO ~')
+    })
+}).then(function (value) {
+    console.log('---------- MyPromise chain callback success 2 ----------')
+    console.log(value);
+    return 100
+}).then(function(value) {
+    console.log('---------- MyPromise chain callback success 2 ----------')
+    console.log(value);
+})
